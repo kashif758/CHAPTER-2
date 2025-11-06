41989dd412891d2c8aa0a08c1a6edec48baf8a6a
kashif758 <kashifmustari@gmail.com>
2025-11-06 23:28:07 +0600

    letel change

--- input.py
+++ input.py
@@ -1,5 +1,5 @@
-a = input("Enter the number 1: ")
-b = input("Enter the number 2: ")
+a = int(input("Enter the number 1: "))
+b = int(input("Enter the number 2: "))
 
 print("Number a is: ", a)
 print("Number b is: ", b)
